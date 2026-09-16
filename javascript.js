577340a9422c1e6db6c4dab2baa3c23d8be28094
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-16 13:31:53 +0900

    Add showEachHand pseudocode

--- javascript.js
+++ javascript.js
@@ -7,7 +7,9 @@
 // コンピュータの勝利数を記録する数字：computerScore
 // プレイヤーの選択した手を保存する数字：playerChoice
 // コンピュータの選択した手を保存する数字：computerChoice
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
 // ランダムにグー、チョキ、パーを返す関数 getComputerChoice
 // グー：０、チョキ：１、パー：２とする
 
@@ -16,12 +18,14 @@
 // 上限の数字max
 
 // Return
-// ０～２のランダムな数字 
+// ０～２のランダムな数字
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
 // プレイヤーが出す手を入力する関数 getPlayerChoice
 
 // プロンプトでrock, scissors, paperのうち一つを入力
-// SWITCH入力に応じて返り値を与える
+// SWITCH：入力に応じて返り値を与える
 // グー：０
 // チョキ：１
 // パー：２
@@ -35,8 +39,41 @@
 // なし
 
 // Return
-// ０～２のランダムな数字 
+// ０～２のランダムな数字
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+// 自分とコンピュータの出した手を表示する関数 showEachHand
+// SWITCH：自分の選択した手の数値によって文字列を代入する
+// CASE０：選択した手の文字列にグーを代入
+// CASE１：選択した手の文字列にチョキを代入
+// CASE２：選択した手の文字列にパーを代入
+// END
+// SWITCH：コンピュータの選択した手の数値によって文字列を代入する
+// CASE０：選択した手の文字列にグーを代入
+// CASE１：選択した手の文字列にチョキを代入
+// CASE２：選択した手の文字列にパーを代入
+// END
+// 自分とコンピュータの出した手の文字列を表示する
+
+// INPUT
+// プレイヤーの選択した手の文字列
+//  コンピュータの選択した手の文字列
+
+// Parameters
+// 自分の選択した手の数値
+// コンピュータの選択した手の数値
+
+//Return
+// 自分とコンピュータの出した手の文字列
+
+// ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+// あいこかを判別して、あいこならもう一度じゃんけんを行う
+// あいこでないなら勝敗判定にうつる
+
+// ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
 // じゃんけんの勝敗を判定する関数 judgeRoundWinner
 
 // プレイヤーから得た数値とコンピュータから得た数値を比べる
@@ -64,101 +101,24 @@
 // IFEND
 
 // Parameters
-// プレイヤーから得た数値 
+// プレイヤーから得た数値
 // コンピュータから得た数値
 
 // Return
 // なし
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
 // ラウンドを記録する
 
 // WHILE
 // ラウンド終了時にcountを1増加させる
-// count < 5まで続ける
+// round < 5まで続ける
 // WHILEEND
+
 // ーーーーーーーーーーーーーーーーーーーーーーーーーー
-// ゲーム全体の勝敗を判定する関数
 
-// 5ラウンド終了後に行う
-// 三項演算子でPlayerScoreとcomputerScoreを比べる
-// 勝者の名前を返す
-
-// Parameters
-// playerScore
-// computerScore
-
-// Return
-// 勝者を発表する文字列
-
-let count = 0;
-let playerScore = 0;
-let computerScore = 0;
-let playerChoice = 0;
-let computerChoice = 0;
-
-function getComputerChoice(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getPlayerChoice() {
-    let playerInput = prompt("あなたの手を入力してください/n rock, scissors, paper","入力されていません");
-    switch (playerInput.toLowerCase()) {
-        case "rock":
-            console.log("グーを選択しました");
-            return 0;
-            break;
-        case "scissors":
-            console.log("チョキを選択しました");
-            return 1;
-            break;
-        case "paper":
-            console.log("パーを選択しました");
-            return 2;
-            break;
-        default:
-            console.log("無効な入力です");
-    }
-}
-
-function judgeRoundWinner(playerChoice, computerChoice) {
-    if(playerChoice === computerChoice) {
-        console.log("あいこです");
-    } else {
-        switch (playerChoice + computerChoice) {
-            case 1:
-                if(playerChoice === 0) {
-                    playerScore += 1;
-                    console.log("あなたの勝利です");
-                } else {
-                    computerScore += 1;
-                    console.log("コンピュータの勝利です");
-                }
-                break;
-            case 2:
-                if(playerChoice === 2) {
-                    playerScore += 1;
-                    console.log("あなたの勝利です");
-                } else {
-                    computerScore += 1;
-                    console.log("コンピュータの勝利です");
-                }
-                break;
-            case 3:
-                if(playerChoice === 1) {
-                    playerScore += 1;
-                    console.log("あなたの勝利です");
-                } else {
-                    computerScore += 1;
-                    console.log("コンピュータの勝利です");
-                }
-                break;
-            default:
-                console.log("エラー");    
-        }
-    }
-}
-
-// ゲーム全体の勝敗を返す関数 gameWinner
+// ゲーム全体の勝者を返す関数 getGameWinner
 
 // 5ラウンド終了後に行う
 // 三項演算子でPlayerScoreとcomputerScoreを比べる
@@ -173,12 +133,99 @@ function judgeRoundWinner(playerChoice, computerChoice) {
 
 // Return
 // 勝者の名前の文字列
-function gameWinner(playerScore, computerScore) {
-    let winnerName = playerScore > computerScore ? "あなた" : "コンピュータ";
-    return winnerName;
+
+// ーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+let round = 0; //ラウンド数を記録する
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice = 0;
+let computerChoice = 0;
+
+function getComputerChoice(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// 動作確認
-playerScore =  4;
-computerScore = 5;
-console.log(gameWinner(playerScore, computerScore));
+function getPlayerChoice() {
+  let playerInput = prompt(
+    "あなたの手を入力してください/n rock, scissors, paper",
+    "入力されていません",
+  );
+  switch (playerInput.toLowerCase()) {
+    case "rock":
+      console.log("グーを選択しました");
+      return 0;
+      break;
+    case "scissors":
+      console.log("チョキを選択しました");
+      return 1;
+      break;
+    case "paper":
+      console.log("パーを選択しました");
+      return 2;
+      break;
+    default:
+      console.log("無効な入力です");
+  }
+}
+
+function showEachHand(playerChoice, computerChoice) {
+  let playerHand;
+  let computerHand;
+}
+
+function judgeRoundWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    console.log("あいこです");
+  } else {
+    switch (playerChoice + computerChoice) {
+      case 1:
+        if (playerChoice === 0) {
+          playerScore += 1;
+          console.log("あなたの勝利です");
+        } else {
+          computerScore += 1;
+          console.log("コンピュータの勝利です");
+        }
+        break;
+      case 2:
+        if (playerChoice === 2) {
+          playerScore += 1;
+          console.log("あなたの勝利です");
+        } else {
+          computerScore += 1;
+          console.log("コンピュータの勝利です");
+        }
+        break;
+      case 3:
+        if (playerChoice === 1) {
+          playerScore += 1;
+          console.log("あなたの勝利です");
+        } else {
+          computerScore += 1;
+          console.log("コンピュータの勝利です");
+        }
+        break;
+      default:
+        console.log("エラー");
+    }
+  }
+}
+
+function getGameWinner(playerScore, computerScore) {
+  let winnerName = playerScore > computerScore ? "あなた" : "コンピュータ";
+  return winnerName;
+}
+
+/*
+console.log("じゃんけんを開始します。");
+
+while (round < 5) {
+    console.log(`ラウンド${round + 1}`);
+    computerChoice = getComputerChoice(0, 2);
+    console.log("最初はグー、じゃんけん...");
+    playerChoice = getPlayerChoice();
+
+    round++;
+}
+*/
